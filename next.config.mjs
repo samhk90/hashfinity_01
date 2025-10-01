ddff13e35e3eed8266f2e17f649c8127e7f40117
samhk90 <mdsami9022@gmail.com>
2025-10-01 19:40:34 +0530

    optimized next config

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+
+output: "export",
+
+basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
+
+assetPrefix: process.env.URL ? process.env.URL : undefined,
+};
+export default nextConfig;
